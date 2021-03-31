d057fc289255594d52e57ef5b85688615b1cf464
TYF1997 <878753035@qq.com>
2021-03-31 11:59:53 +0800

    提交

--- tyf.sql
+++
@@ -1,4 +0,0 @@
-update esignaturelogic e
-set e.elexpression = '${appntBirthday}>=60' || ''||'&'||'&'||''
-|| '${signCity}==''310100''' || ''||'&'||'&'||''
-|| '${INS_INSH_1161_1}' where e.filetypecode = 'INS-INSH-1161-1'
